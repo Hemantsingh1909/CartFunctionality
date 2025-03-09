2971a2a4e31820c065dc47235698567f462baa95
Hemant Satwal <hemantsingh1909@gmail.com>
2025-03-09 13:02:40 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// filepath: /Users/hemantsingh/Developer/cart_functionality/vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/CartFunctionality/",
   plugins: [react()],
   server: {
     open: true,
